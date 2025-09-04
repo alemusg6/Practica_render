f0e64dc89cc6eff3cf5c61a4ce6c8b4519d10c7e
alemusg6 <105267587+alemusg6@users.noreply.github.com>
2025-09-03 22:53:13 -0600

    Update db.js

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }  // 🔑 Render requiere SSL
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false } // necesario en Render
 });
 
 module.exports = pool;
